87de228dc8abc0d0b86c72425fb6a570b353729b
Damiie Gregorio <damiiekeith@gmail.com>
2017-09-19 15:28:34 +0800

    layout for index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,42 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<h1>{{ $title }}</h1>
-<p>
-	@if(count($chars)>0)
-		@foreach($chars as $char)
-			<ul class="list-group">
-				<li class="list-group-item">{{ $char }}</li>
-			</ul>
-			
-		@endforeach
-	@endif
-</p>
-@endsection
+         <title>TUP - Accreditation Document Management System</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{!! asset('css/app.css') !!}" media="all" rel="stylesheet" type="text/css" />
+        <link href="{!! asset('css/style.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+       
+    </head>
+    <body>
+    	<section id="">
+    		
+    	</section>
+        <div class="col-sm-12 home-header">
+	        	<div class="col-md-4 col-sm-4 col-xs-6 home-logo">
+	               <img src="{{ URL::to('/images/tup-logo.png') }}">
+	            </div>
+	            <div class="col-md-4 col-sm-4 col-xs-6 home-name">
+	                <p>ACCREDITATION <br />DOCUMENT MANAGEMENT <br /> SYSTEM</p>
+	            </div>
+	              @if (Route::has('login'))
+                <div class="col-md-4 col-sm-4 col-xs-12 home-login-reg">
+                    @auth
+                        <button class="">
+                            <a href="{{ url('/home') }}">Home</a>
+                        </button>
+                    @else
+                        <button class="login-btn"><a href="{{ route('login') }}">Login</a></button>
+                        <button class="reg-btn"><a href="{{ route('register') }}">Register</a></button>
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </body>
+</html>
