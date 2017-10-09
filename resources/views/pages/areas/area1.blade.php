cb0da11ee041af6bedc1161d447449f49d2849ba
Ian Del Carmen <indelcarmen24@gmail.com>
2017-10-09 13:17:42 +0800

    moana

---
+++ resources/views/pages/areas/area1.blade.php
@@ -0,0 +1,45 @@
+
+@extends('layouts.app')
+		
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="panel panel-default">
+		  <div class="panel-heading">Area 1: Vision, Mission, Goals and Objectives</div>
+		  <div class="panel-body">
+		  	<table class="table table-hover">
+			    <thead>	
+			      <tr>
+			        <th>File Name</th>
+			        <th>Date Uploaded</th>
+			        <th>Actions</th>
+			        <th>Select</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			   		<td><a href="">afaafs</a></td>
+			   		<td>Date</td>
+			   		<td>
+			   			<a class="col-md-2" href="" download=""><span class="glyphicon glyphicon-download">
+			   			</span> Download</a>
+			   			<a class="col-md-2" href=""><span class="glyphicon glyphicon-upload"></span> Revise</a>
+			   			<a class="col-md-2" href=""><span class="glyphicon glyphicon-eye-open"></span> View</a>
+			   			<a class="col-md-2" href="">
+			   				<span class="glyphicon glyphicon-qrcode"></span> Scan
+			   			</a>
+			   			<a class="col-md-2" href=""><span class="glyphicon glyphicon-trash"></span> Delete</a>
+			   			
+
+			   		</td>
+			   		<td>
+			   			asda
+			   		</td>
+			      </tr>
+			    </tbody>
+			</table>
+		  </div>
+		</div>
+	</div>
+</div>
+@endsection
